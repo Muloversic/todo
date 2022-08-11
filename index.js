12b46718df114228e29e28a6161b80d3473bda81
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-08-11 19:13:38 +0300

    fixed: rerender todos, changed adding item to todo obj, changed generation of todo id

--- index.js
+++ index.js
@@ -50,28 +50,40 @@ const processForm = (input, button) => {
     e.preventDefault();
     // call createTodo function if input is not epmty string and reset 'todo name + input.value'
     if (todoName) {
-      const todoObj = {};
-      todoObj.name = todoName;
-      todoObj.active = true;
-      const todoId = Math.random().toString().slice(2) + todoName.slice(0, 5);
-      todoObj.id = todoId;
-      todosArray.push(todoObj);
-      createTodoElement(todoId);
+      const todoId = todosArray.length + 1;
+      const todoObj = {
+        name: todoName,
+        active: true,
+        id: todoId,
+      };
+
+      if (!todoInfoBar.children.length) {
+        createInfoBarElements();
+      }
+
       input.classList.remove('todo__form-input--error');
+
+      todosArray.push(todoObj);
+      renderTodos(todosArray);
     }
 
-    if (!todoName) input.classList.add('todo__form-input--error');
+    if (!todoName) {
+      input.classList.add('todo__form-input--error');
+    }
 
     input.value = '';
     todoName = '';
   });
 };
 
-const createTodoInfoBar = () => {
+const createTodoInfoBarContainer = () => {
   // _todo info bar
   const todoInfoBar = document.createElement('div');
   todoInfoBar.classList.add('todo__info-bar');
+  return todoInfoBar;
+};
 
+const createInfoBarElements = () => {
   // _todos counter at info bar
   const todoItemCounter = document.createElement('p');
   todoItemCounter.classList.add('todo__item-counter');
@@ -85,7 +97,6 @@ const createTodoInfoBar = () => {
 
   // append 'counter' and 'remove todo button' to info bar
   todoInfoBar.append(...[todoItemCounter, removeAllButton]);
-  return todoInfoBar;
 };
 
 const createTodosContainer = () => {
@@ -96,19 +107,19 @@ const createTodosContainer = () => {
 
 const todoBody = createTodoTemplate();
 const todoContainer = createTodosContainer();
-const todoInfoBar = createTodoInfoBar();
+const todoInfoBar = createTodoInfoBarContainer();
 
 function removeAllTodo() {
   todosArray = [];
-  changeTodoCounter(todosArray.length);
-  todoContainer.innerHTML = '';
-  todoBody.removeChild(todoContainer);
-  todoBody.removeChild(todoInfoBar);
+  [...todoInfoBar.children].forEach((element) => element.remove());
+  renderTodos(todosArray);
 }
 
 const changeTodoCounter = (num) => {
   const counterItem = document.querySelector('.todo__item-counter');
-  if (counterItem) counterItem.textContent = `Active todos: ${num}`;
+  if (counterItem) {
+    counterItem.textContent = `Active todos: ${num}`;
+  }
 };
 
 const countActiveTodos = (todosArr) => {
@@ -116,60 +127,67 @@ const countActiveTodos = (todosArr) => {
   return activeTodos.length;
 };
 
-const createTodoElement = (currentTodoId) => {
-  // prevent duplicate render
-  const todosToRender = todosArray.filter((todo) => todo.id === currentTodoId);
-  //render todos
-  todosToRender.forEach((todo) => {
-    const todoWrapper = document.createElement('div');
-    todoWrapper.classList.add('todo__element-wrapper');
-    todoWrapper.id = todo.id;
+const renderTodos = (todos) => {
+  [...todoContainer.children].forEach((todo) => todo.remove());
+  todos.forEach((todo) => createTodoElement(todo));
+};
 
-    // create input and its attrs
-    const todoItem = document.createElement('input');
-    todoItem.classList.add('todo__element');
-    todoItem.value = todo.name;
-    todoItem.name = todo.name;
-    todoItem.disabled = true;
+const createTodoElement = (todo) => {
+  //render todo
+  const todoWrapper = document.createElement('div');
+  todoWrapper.classList.add('todo__element-wrapper');
+  todoWrapper.id = todo.id;
 
-    // add toggle todo status listener for todo wrapper
-    todoWrapper.addEventListener('click', (e) => toggleTodoStatus(e.target, todo.id, todoItem.disabled));
+  // create input and its attrs
+  const todoItem = document.createElement('input');
+  todoItem.classList.add('todo__element');
+  todoItem.value = todo.name;
+  todoItem.name = todo.name;
+  todoItem.disabled = true;
+  if (!todo.active) {
+    todoItem.classList.add('todo__element--done');
+  }
 
-    // create edit button
-    const editButton = document.createElement('span');
-    editButton.classList.add(...['todo__edit', 'todo__action-element']);
-    editButton.innerHTML = '&#9998;';
-    let isEditing = false;
+  // add toggle todo status listener for todo wrapper
+  todoWrapper.addEventListener('click', (e) => toggleTodoStatus(e.target, todo.id, todoItem.disabled));
 
-    // listener for edit button
-    editButton.addEventListener('click', () => {
-      // !isEditing means input is editable
-      if (todoItem.value !== '') isEditing = !isEditing;
-      editTodo(editButton, todoItem, isEditing, todo.id);
+  // create edit button
+  const editButton = document.createElement('span');
+  editButton.classList.add(...['todo__edit', 'todo__action-element']);
+  editButton.innerHTML = '&#9998;';
+  let isEditing = false;
 
-      // flip isEditing state if esc was pressed
-      document.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape') {
-          isEditing = false;
-        }
-      });
+  // listener for edit button
+  editButton.addEventListener('click', () => {
+    // _!isEditing means input is editable
+    if (todoItem.value !== '') {
+      isEditing = !isEditing;
+    }
+    editTodo(editButton, todoItem, isEditing, todo.id);
+
+    // flip isEditing state if esc was pressed
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        isEditing = false;
+      }
     });
-
-    // create delete button
-    const deleteButton = document.createElement('span');
-    deleteButton.classList.add(...['todo__delete', 'todo__action-element']);
-    deleteButton.textContent = 'x';
-    // listener for delete button
-    deleteButton.addEventListener('click', () => deleteTodo(todo.id, todoWrapper));
-
-    // append todo item+action button to wrapper, append wrappers to container
-    todoWrapper.append(...[todoItem, editButton, deleteButton]);
-    todoContainer.append(todoWrapper);
   });
 
+  // create delete button
+  const deleteButton = document.createElement('span');
+  deleteButton.classList.add(...['todo__delete', 'todo__action-element']);
+  deleteButton.textContent = 'x';
+  // listener for delete button
+  deleteButton.addEventListener('click', () => deleteTodo(todo.id));
+
+  // append todo item+action button to wrapper, append wrappers to container
+  todoWrapper.append(...[todoItem, editButton, deleteButton]);
+  todoContainer.append(todoWrapper);
+
   changeTodoCounter(countActiveTodos(todosArray));
-  todoBody.append(...[todoInfoBar, todoContainer]);
 };
+
+todoBody.append(...[todoInfoBar, todoContainer]);
 
 const toggleTodoStatus = (target, todoId, isDisabled) => {
   const isTodo = [...target.classList].includes('todo__element');
@@ -222,14 +240,15 @@ const editTodo = (button, todoItem, isEditing, todoId) => {
   }
 };
 
-const deleteTodo = (todoId, todoElement) => {
+const deleteTodo = (todoId) => {
   todosArray = todosArray.filter((todo) => todo.id !== todoId);
-  const todoToRemove = document.getElementById(todoId);
-  todoElement.remove(todoToRemove);
+  renderTodos(todosArray);
   changeTodoCounter(countActiveTodos(todosArray));
 
   // if array with todos is empty - delete container with them and info bar
-  if (todosArray.length === 0) removeAllTodo();
+  if (todosArray.length === 0) {
+    removeAllTodo();
+  }
 };
 
 app.append(todoBody);
