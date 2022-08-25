52a29e44290bcc7cc44a350f115562155e6f6ec7
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-08-25 16:46:38 +0300

    replaced import

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import mongoose from 'mongoose';
+import postTodo from './controllers/Todos/createTodo';
 
 const db = 'mongodb://localhost:27017/todo';
 const connectDb = async () => {
@@ -12,8 +13,6 @@ const connectDb = async () => {
 };
 connectDb();
 
-import postTodo from './controllers/Todos/createTodo';
-
 const PORT = 8080;
 const server = http
   .createServer((request, response) => {
@@ -21,7 +20,6 @@ const server = http
     if (request.method === 'OPTIONS') {
       response.setHeader('Access-Control-Allow-Origin', '*');
       response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-	  response.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
       response.writeHead(200);
       response.end();
     }
