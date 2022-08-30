354f29259c147c4d15417bed68926ca2c259b208
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-08-30 12:11:15 +0300

    added rule for todo model

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -5,7 +5,7 @@ const Todo = new mongoose.Schema(
     name: { type: String, required: true },
     active: { type: Boolean, required: true },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 )
 
 export default mongoose.model('Todo', Todo)
