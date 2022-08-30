8ad12c07e13ab09bb654c33937cb8031d641a3d4
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-08-30 12:30:40 +0300

    added koa logger

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import Koa from 'koa'
+import KoaLogger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
 import router from './router'
@@ -19,6 +20,7 @@ const PORT = 8080
 const app = new Koa()
 app.use(cors())
 app.use(bodyParser())
+app.use(KoaLogger())
 app.use(router.routes())
 app.use(router.allowedMethods())
 app.listen(PORT, () => {
