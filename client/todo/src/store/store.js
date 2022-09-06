a8e994474290ea88d52d71992bbcf9ba67256ead
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-09-06 17:23:32 +0300

    installed redux logger

--- client/todo/src/store/store.js
+++ client/todo/src/store/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from '@redux-saga/core'
+import Logger from 'redux-logger'
 import rootReducer from './rootReducer'
 import saga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, Logger))
 sagaMiddleware.run(saga)
 export default store
