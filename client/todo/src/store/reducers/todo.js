adcaf546fb1c826cac78cd0b25e38903fd9132af
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-09-08 19:06:33 +0300

    returned action imports

--- client/todo/src/store/reducers/todo.js
+++ client/todo/src/store/reducers/todo.js
@@ -1,4 +1,12 @@
 import { handleAction, handleActions } from 'redux-actions'
+import {
+  LOAD_TODO_SUCCESS,
+  ADD_TODO_SUCCESS,
+  DELETE_TODO_SUCCESS,
+  UPDATE_TODO_SUCCESS,
+  UPDATE_FILTER,
+  DELETE_ALL_TODOS_SUCCESS,
+} from '../../constants'
 
 const TODOS_STATE = []
 const TODOS_FILTER = 'all'
@@ -7,17 +15,17 @@ export const filter = handleAction(UPDATE_FILTER, (state, { payload }) => payloa
 
 export const todo = handleActions(
   {
-    LOAD_TODO_SUCCESS: (state, { payload }) => payload.data,
-    ADD_TODO_SUCCESS: (state, { payload }) => {
+    [LOAD_TODO_SUCCESS]: (state, { payload }) => payload.data,
+    [ADD_TODO_SUCCESS]: (state, { payload }) => {
       if (payload.filterType !== 'done') {
         return [...state, payload.data]
       }
 
       return TODOS_STATE
     },
-    DELETE_TODO_SUCCESS: (state, { payload }) => state.filter((todo) => todo._id !== payload._id),
-    DELETE_ALL_TODOS_SUCCESS: () => [],
-    UPDATE_TODO_SUCCESS: (state, { payload }) => {
+    [DELETE_TODO_SUCCESS]: (state, { payload }) => state.filter((todo) => todo._id !== payload._id),
+    [DELETE_ALL_TODOS_SUCCESS]: () => [],
+    [UPDATE_TODO_SUCCESS]: (state, { payload }) => {
       const { _id, name, active } = payload.todos
       const { filterType } = payload
       const shouldTodoRemove =
